ee0dc2476482276cf00cf3bfee1044c20751b756
Yuan Gabriel Lacambra <yuangabrielslacambra@iskolarngbayan.pup.edu.ph>
2022-07-08 17:05:43 +0800

    Changed the way the data is structured in the JSON File. Also removed the method and instead placed the info that's gonna be written on the JSON file to the form load event.

--- PDFResumeGenerator/PDFResumeGeneratorForm.cs
+++ PDFResumeGenerator/PDFResumeGeneratorForm.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace PDFResumeGenerator
 {
     public partial class PDFResumeGeneratorForm : Form
@@ -6,14 +9,19 @@ namespace PDFResumeGenerator
         {
             InitializeComponent();
         }
-        class PersonalInformation
+        class ResumeInfo
         {
             public string Name { get; set; }
             public int Age { get; set; }
             public string Address { get; set; }
             public string Nationality { get; set; }
             public string Religion { get; set; }
-            public string Profile { get; set; } 
+            public string Profile { get; set; }
+            public List<ContactInformation> ContactInfo { get; set; }
+            public List<SkillsAndQualities> QualitiesaAndSkills { get; set; }
+            public List<School> EducationalAttainment { get; set; }
+            public List<InternshipExperience> Internship{ get; set; }
+
         }
         class ContactInformation
         {
@@ -26,11 +34,7 @@ namespace PDFResumeGenerator
             public List<string> Skills { get; set; }
             public List<string> Qualities { get; set; }
         }
-        class Education
-        {
-            public List<School> SchoolsAttended { get; set; }
-        }
-        class  School
+        class School
         {
             public string SchoolName { get; set; }
             public string YearAttended { get; set; }
@@ -43,56 +47,60 @@ namespace PDFResumeGenerator
             public string Job { get; set; }
             public List<string> Experience { get; set; }
         }
+      
+           
+        
+    
         private void PDFResumeGeneratorForm_Load(object sender, EventArgs e)
         {
-           
-        }
-
-        private void ResumeInfo()
-        {
-            PersonalInformation PersonalInfo = new PersonalInformation()
+            ResumeInfo ResumeInformation = new ResumeInfo()
             {
                 Name = "Yuan Gabriel S. Lacambra",
                 Age = 21,
                 Address = "#3 Cojuangco St., Everhills Subdivision, Brgy. Inarawan, Antipolo City, Rizal",
                 Nationality = "Filipino",
                 Religion = "Agnostic",
-                Profile = "Motivated recent college graduate aspiring to become a computer engineer. Looking for a company where I can use my educational background to develop and hone my skills."
-            };
-            ContactInformation ConatctInfo = new ContactInformation()
-            {
-                ContactNumber = 09956783420,
-                EmailAddress = "yuangab10@gmail.com"
-            };
-            SkillsAndQualities SkillAndQuality = new SkillsAndQualities()
-            {
-                Skills = new List<string>()
+                Profile = "Motivated recent college graduate aspiring to become a computer engineer. Looking for a company where I can use my educational background to develop and hone my skills.",
+                ContactInfo = new List<ContactInformation>
                 {
-                    "Artificial Intelligence and Machine Learning",
-                    "Edge Computing",
-                    "Computational Algorithms",
-                    "Operating Systems",
-                    "Thorough understanding of the.NET technology and developing Java apps.",
-                    "Strong programming and analytical abilities",
-                    "Strong familiarity with database management systems including Oracle, MS Access, and MySQL.",
-                    "Well-Versed at 3 Programming Languages (C#, C++, Python)",
+                    new ContactInformation()
+
+                    {
+                        ContactNumber = 09956783420,
+                        EmailAddress = "yuangab10@gmail.com"
+                    }
                 },
-                Qualities = new List<string>()
+                QualitiesaAndSkills = new List<SkillsAndQualities>
                 {
-                    "Meticulous and Organized",
-                    "Fast Learner. Can learn new technologies and skills quickly.",
-                    "High technical competence",
-                    "Highly dependable and adaptable",
-                    "Logical and analytical thinker. Incredibly adept at solving problems",
-                    "Amiable and gets along with everyone",
-                    "Diligent and highly persistent"
-                }
-            };
-            Education EducationAttained = new Education()
-            {
-                SchoolsAttended = new List<School>
+                    new SkillsAndQualities()
+                    {
+                        Skills = new List<string>()
+                        {
+                            "Artificial Intelligence and Machine Learning",
+                            "Edge Computing",
+                            "Computational Algorithms",
+                            "Operating Systems",
+                            "Thorough understanding of the.NET technology and developing Java apps.",
+                            "Strong programming and analytical abilities",
+                            "Strong familiarity with database management systems including Oracle, MS Access, and MySQL.",
+                            "Well-Versed at 3 Programming Languages (C#, C++, Python)",
+                        },
+                        Qualities = new List<string>()
+                        {
+                            "Meticulous and Organized",
+                            "Fast Learner. Can learn new technologies and skills quickly.",
+                            "High technical competence",
+                            "Highly dependable and adaptable",
+                            "Logical and analytical thinker. Incredibly adept at solving problems",
+                            "Amiable and gets along with everyone",
+                            "Diligent and highly persistent"
+                        }
+                    }
+
+                },
+                EducationalAttainment = new List<School>
                 {
-                    new School
+                    new School()
                     {
                         SchoolName = "Fuschia High School",
                         YearAttended = "2013-2019",
@@ -102,7 +110,7 @@ namespace PDFResumeGenerator
                             "District-Level Quizbee Winner"
                         }
                     },
-                    new School
+                    new School()
                     {
                         SchoolName = "Alfaroa College",
                         YearAttended = "2019-2023",
@@ -112,22 +120,27 @@ namespace PDFResumeGenerator
                             "President's Lister"
                         }
                     }
-                }
-            };
-            InternshipExperience Internship = new InternshipExperience()
-            {
-                Company = "JAVA Systems Philippiones",
-                Duration = "2021-2023",
-                Job = "Computer Engineering",
-                Experience = new List<string>()
-
+                },
+                Internship = new List<InternshipExperience>
                 {
-                    "Helped with project testing, evaluation, debugging, and documentation.",
-                    "Implemented and troubleshot the wireless and LAN Network.",
-                    "Created and tested operating systems and firmware.",
-                    "Participated in code and design reviews."
+                    new InternshipExperience()
+                    {
+                        Company = "JAVA Systems Philippiones",
+                        Duration = "2021-2023",
+                        Job = "Computer Engineering",
+                        Experience = new List<string>()
+                        {
+                            "Helped with project testing, evaluation, debugging, and documentation.",
+                            "Implemented and troubleshot the wireless and LAN Network.",
+                            "Created and tested operating systems and firmware.",
+                            "Participated in code and design reviews."
+                        }
+
+                    }
                 }
             };
         }
+
+      
     }
 }
